c3c7c5904a2ff147487af80419e3d68b517722b2
StefanMoosmann <smo1982@gmx.at>
2020-06-08 14:48:13 +0200

    first insert

---
+++ ObjektGenerator/ObjectGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjektGenerator
+{
+    class ObjectGenerator
+    {
+        public T GetNewObject<T>() where T : new()
+        {
+            T newObjekt = new T();
+
+            return newObjekt;
+        }
+
+        public List<T> GetNewObjects<T>(int amountOfObjects) where T : new()
+        {
+            List<T> list = new List<T>();
+
+            for(int i = 0; i < amountOfObjects; i++)
+            {
+                T newObjekt = GetNewObject<T>();
+                list.Add(newObjekt);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ ObjektGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjektGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ObjectGenerator generator = new ObjectGenerator();
+            Car car = generator.GetNewObject<Car>();
+            List<Car> newList = generator.GetNewObjects<Car>(5);
+        }
+    }
+}
